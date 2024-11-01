d67bfbc1240cf1c5d4c343df1401ba6683b250c6
Daniel Azulay <daniel@danielazulay.eu>
2024-11-01 02:16:19 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,7 @@ if [ ! -d lib/pugixml ] ; then
     git clone https://github.com/zeux/pugixml.git lib/pugixml
     pushd lib/pugixml
     git checkout latest
+    cmake .
     popd
 fi
 
@@ -12,8 +13,8 @@ fi
 if [ ! -d lib/updfparser ] ; then
     git clone https://github.com/SamuelMarks/updfparser.git lib/updfparser
     pushd lib/updfparser
-    make BUILD_STATIC=1 BUILD_SHARED=0
-    cp libupdfparser.a /usr/lib/
+    make BUILD_STATIC=1 BUILD_SHARED=1
+    cp libupdfparser.o /usr/lib/
     ldconfig
     popd
 fi
